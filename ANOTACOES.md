0d7765440baa28abb75c03ecf4c861f44b760215
Cicero Feijo <cicero.feijo@gmail.com>
2023-07-17 22:11:39 -0300

    A01: configuração do TailwindCSS

--- ANOTACOES.md
+++ ANOTACOES.md
@@ -1,7 +1,3 @@
-[X] Criação do REPO
-[X] Criou a React App
-[X] Configuração do Ambiente (lint e vscode)
-[X] Limpa no desnecessário
 
 
 
@@ -9,11 +5,3 @@
 
 
 
-
-
-
-
-
-
-
-https://bobbyhadz.com/blog/unable-to-load-schema-from-vscode-cannot-open
--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Ola</h1>
+    <>
+      <div className="p-6 bg-gray-400 border border-b-orange-500">
+        <h1 className='text-orange-500 font-bold'>Ola</h1>
+      </div>
+      <button 
+        type="button"
+        className="bg-blue-500 text-white rounded px-10 py-6">
+          Clique Aqui
+      </button>
+    </>
   )
 }
 
